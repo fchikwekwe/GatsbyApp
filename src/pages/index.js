ae94fb87c04f689b43352749629b23bdc76cd8c1
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-09-02 12:49:40 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello world!</h1>
+    <p>My name is Faith Chikwekwe and I am a software developer in training.</p>
+    <p>I am excited to show you all what I can make.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
